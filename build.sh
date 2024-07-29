38f01ea01abe6f6a825c4cee5a3ed8a7b9a33403
MirandaYuber <mirandayuber7@gmail.com>
2024-07-28 19:26:41 -0500

    Actualización de cv

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
+source venv/bin/activate
+pip install -upgrade pip
 pip install -r requirements.txt
-rm -rf public
 reflex init
 reflex export --frontend-only
+rm -rf public
 unzip frontend.zip -d public
-rm -f frontend.zip
-deactivate
+rm -rf frontend.zip
